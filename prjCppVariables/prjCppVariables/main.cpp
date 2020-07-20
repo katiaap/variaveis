21f2f8a8ee772e5fff48d24b5af5bc1e28b2cbab
Your Name <silva81.katia@gmail.com>
2020-07-20 09:10:16 -0300

    exemplos de variaveis

--- prjCppVariables/prjCppVariables/main.cpp
+++ prjCppVariables/prjCppVariables/main.cpp
@@ -2,6 +2,18 @@
 
 using namespace std;
 void main (){
+	string fName, lName;
+	short age;
+
+	cout << "Enter first Name: ";
+	cin >> fName;
+	cout << "Enter last Name: ";
+	cin >> lName;
+	cout << "Enter your age: ";
+	cin >> age;
+	cout << "\n\n Hello " << fName << " " << lName << ",\n";
+	cout << "You are " << age << " years old.";
+
 
 
 
